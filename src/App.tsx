205f7551d603e6bac03edb25dc8f18f8e2afefb0
Martin <martingalindo3202@gmail.com>
2025-08-10 20:38:44 -0600

    Tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css'
 
 function App () {
   return (
-    <div>
-      HOLA
+ <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        ¡Tailwind funciona!
+      </h1>
     </div>
   )
 }
